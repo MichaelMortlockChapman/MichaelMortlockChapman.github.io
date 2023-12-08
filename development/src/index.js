1b301ec1de3b7bc62a95d0f23f6ae4c1abc8e659
Michael Mortlock-Chapman (z5359078) <z5359078@unsw.edu.au>
2023-12-08 19:34:58 +1100

    update site title

--- development/src/index.js
+++ development/src/index.js
@@ -4,6 +4,7 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import './index.css'
 
+document.title = 'Michael Mortlock-Chapman Portfolio Site'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
