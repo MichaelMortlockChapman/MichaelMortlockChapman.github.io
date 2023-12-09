78653b5b44d66f08462a14c55af943972606415f
Michael Mortlock-Chapman (z5359078) <z5359078@unsw.edu.au>
2023-12-09 17:01:12 +1100

    changed bash script to set title and remove desc

--- development/build_setup.sh
+++ development/build_setup.sh
@@ -2,3 +2,10 @@
 
 echo 'npm run build'
 cp -TRv build/ ../
+# <meta name="description" content="Web site created using create-react-app"/>
+var1='<meta name="description" content="Web site created using create-react-app"\/>'
+var2=''
+sed -i -e "s/$var1/$var2/" ../index.html
+var1='<title>React App<\/title>'
+var2='<title>Michael Mortlock-Chapman Portfolio Site<\/title>'
+sed -i -e "s/$var1/$var2/" ../index.html
