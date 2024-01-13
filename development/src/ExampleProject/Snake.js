61d3167665a5b7ac2e8449fe7ba4ac95d3ca4ea7
Michael Mortlock-Chapman (z5359078) <z5359078@unsw.edu.au>
2024-01-13 19:39:43 +1100

    added game settings to snake game

--- development/src/ExampleProject/Snake.js
+++ development/src/ExampleProject/Snake.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import { useEffect, useRef, useState } from 'react'
-import { Button, Stack, Typography } from '@mui/material'
+import { Button, Slider, Stack, Typography } from '@mui/material'
 import Radio from '@mui/material/Radio'
 import RadioGroup from '@mui/material/RadioGroup'
 import FormControlLabel from '@mui/material/FormControlLabel'
@@ -10,9 +10,51 @@ import FormLabel from '@mui/material/FormLabel'
 import GameBoi from './GameBoi'
 import { useTheme } from '@emotion/react'
 
+// GAME TITLE TEXT AND ELEMENT
+const snakeText = `
+ __  _  _   _   _  _ ___\n
+/ _|| \\| | / \\ | |//| __|\n
+\\_ \\| \\\\ || o ||  ( | _|\n 
+|__/|_|\\_||_n_||_|\\\\|___|\n  
+`
+const SnakeTitle = () => <div style={{ fontFamily: 'monospace', whiteSpace: 'pre', lineHeight: '0.5' }}>{snakeText}</div>
+
+// GAME SPEEDS
+const gameRate = 1000 / 60 // 1/60s
+const baseGameSpeed = 1000
+const gameSpeeds = [
+  {
+    value: 1,
+    speed: 2,
+    label: 'x1/2'
+  },
+  {
+    value: 2,
+    speed: 1,
+    label: ''
+  },
+  {
+    value: 3,
+    speed: 1 / 2,
+    label: 'x2'
+  },
+  {
+    value: 4,
+    speed: 1 / 3,
+    label: 'x3'
+  },
+  {
+    value: 5,
+    speed: 1 / 10,
+    label: 'x10'
+  }
+]
+
+// REACT SNAKE GAME ELEMENT
 export default function Snake () {
   const theme = useTheme()
 
+  // GRID FUNCS FOR 2D ARR AND ELEMENT
   const width = 20
   const height = 20
 
@@ -60,12 +102,14 @@ export default function Snake () {
     )
   }
 
+  // GAME SCREEN
   const [grid, setGrid] = useState(<></>)
-
+  // GAME VARS
   const gridArr = useRef(createGridArr(width, height))
   const snake = useRef([])
   const direction = useRef(([0, 1])) 
  
+  // GAME LOGIC FUNCS
   const placeNewApple = (gridArr) => {
     const randomInt = (max) => Math.floor(Math.random() * max)
     let aRandomTile = [randomInt(width), randomInt(height)]
@@ -80,7 +124,6 @@ export default function Snake () {
     case 2:
       // eslint-disable-next-line no-case-declarations
       const newTail = snake[snake.length - 1].last
-      console.log(newTail)
       snake.push({ now: newTail, last: newTail })
       // eslint-disable-next-line no-case-declarations
       const [x, y] = newTail
@@ -96,46 +139,64 @@ export default function Snake () {
     }
   }
 
+  // GAME SETTINGS
   const [portals, setPortals] = useState('off')
   const handlePortalSelection = (e) => { setPortals(e.target.value) }
 
+  const [speed, setSpeed] = useState(1)
+  const handleSpeedChange = (e) => {
+    setSpeed(e.target.value)
+  }
+
+  // GAME UPDATGE LOOP FUNC
   const [pausedState, setPausedState] = useState(false)
   const paused = useRef(false)
   const update = () => {
     console.log('update')
-    if (paused.current) {
+    // using 'clock' as self-calling timeout was buggy so running update at 100ms and doing 'frame' when needed
+    if (timerClock.current++ < (gameSpeeds[speed - 1].speed * baseGameSpeed) / gameRate) {
       return
     }
-    const _gridArr = JSON.parse(JSON.stringify(gridArr.current))
-    const _snake = JSON.parse(JSON.stringify(snake.current))
-    for (let i = 0; i < _snake.length; i++) {
-      const [x, y] = _snake[i].now
-      _gridArr[x][y] = 0
-      _snake[i].last = _snake[i].now
+    timerClock.current = 0
+    if (!paused.current) {
+      // normal game loop logic
+      const _gridArr = JSON.parse(JSON.stringify(gridArr.current))
+      const _snake = JSON.parse(JSON.stringify(snake.current))
+      for (let i = 0; i < _snake.length; i++) {
+        const [x, y] = _snake[i].now
+        _gridArr[x][y] = 0
+        _snake[i].last = _snake[i].now
+      }
+      for (let i = 1; i < _snake.length; i++) {
+        const [x, y] = _snake[i - 1].last
+        _gridArr[x][y] = 1
+        _snake[i].now = _snake[i - 1].last
+      }
+      const [x, y] = _snake[0].now
+      const [dx, dy] = direction.current
+      if (portals === 'off' && ((x + dx) >= width || (x + dx) < 0 || (y + dy) >= height || (y + dy) < 0)) {
+        handleGameOver()
+        return
+      }
+      let newX = (x + dx) % width
+      let newY = (y + dy) % height
+      newX = newX < 0 ? (width - 1) : newX
+      newY = newY < 0 ? (height - 1) : newY
+      _snake[0].now = [newX, newY]
+      titleCheck(_gridArr, _snake, _gridArr[newX][newY])
+      _gridArr[newX][newY] = 1
+      
+      snake.current = _snake
+      gridArr.current = _gridArr
+      setGrid(createGrid(_gridArr))
     }
-    for (let i = 1; i < _snake.length; i++) {
-      const [x, y] = _snake[i - 1].last
-      _gridArr[x][y] = 1
-      _snake[i].now = _snake[i - 1].last
-    }
-    const [x, y] = _snake[0].now
-    const [dx, dy] = direction.current
-    let newX = (x + dx) % width
-    let newY = (y + dy) % height
-    newX = newX < 0 ? (width - 1) : newX
-    newY = newY < 0 ? (height - 1) : newY
-    _snake[0].now = [newX, newY]
-    titleCheck(_gridArr, _snake, _gridArr[newX][newY])
-    _gridArr[newX][newY] = 1
-    
-    snake.current = _snake
-    gridArr.current = _gridArr
-    setGrid(createGrid(_gridArr))
   }
 
+  // GAME MENU AND INPUT HANDLING
   const [started, setStarted] = useState(false)
   const [gameOver, setGameOver] = useState(false)
   const timerID = useRef(-1)
+  const timerClock = useRef(0)
 
   const handleStartGame = () => {
     setGameOver(false)
@@ -143,9 +204,10 @@ export default function Snake () {
     direction.current = [0, 1] 
     snake.current = [{ now: [9, 7], last: [9, 6] }]
     gridArr.current[9][7] = 1
-    gridArr.current[9][9] = 2
+    // gridArr.current[9][9] = 2
+    placeNewApple(gridArr.current)
     setGrid(createGrid(gridArr.current))
-    timerID.current = setInterval(update, 1000)
+    timerID.current = setInterval(update, gameRate)
     setStarted(true)
   }
   const handleGameOver = () => {
@@ -168,20 +230,6 @@ export default function Snake () {
     }
   }, [])
 
-  const handleKeyPress = (e) => {
-    let newDir = direction.current
-    if (e.key === 'ArrowRight') {
-      newDir = [0, 1]
-    } else if (e.key === 'ArrowLeft') {
-      newDir = [0, -1]
-    } else if (e.key === 'ArrowUp') {
-      newDir = [-1, 0]
-    } else if (e.key === 'ArrowDown') {
-      newDir = [1, 0]
-    }
-    direction.current = newDir
-  }
-
   const menuStyle = {
     position: 'absolute',
     top: '50%',
@@ -202,19 +250,51 @@ export default function Snake () {
     }
   }
 
+  const willMoveIntoSelf = (newDir) => {
+    const h = direction.current[1] * newDir[1]
+    const v = direction.current[0] * newDir[0]
+    // ignore when snake is 1 tile long
+    return (h === -1 || v === -1) && snake.current.length > 1
+  }
+  
+  const handleChangeDir = (newDir) => {
+    // ignore moves that move 'backwards' into self for game 'fairness'
+    if (willMoveIntoSelf(newDir)) {
+      return
+    }
+    direction.current = newDir
+  }
+
+  const handleKeyPress = (e) => {
+    let newDir = direction.current
+    if (e.key === 'ArrowRight') {
+      newDir = [0, 1]
+    } else if (e.key === 'ArrowLeft') {
+      newDir = [0, -1]
+    } else if (e.key === 'ArrowUp') {
+      newDir = [-1, 0]
+    } else if (e.key === 'ArrowDown') {
+      newDir = [1, 0]
+    }
+    handleChangeDir(newDir)
+  }
+
   return (
     <>
       <GameBoi
-        handleUpButton={() => { direction.current = [-1, 0] }}
-        handleDownButton={() => { direction.current = [1, 0] }}
-        handleRightButton={() => { direction.current = [0, 1] }}
-        handleLeftButton={() => { direction.current = [0, -1] }}
+        handleUpButton={() => { handleChangeDir([-1, 0]) }}
+        handleDownButton={() => { handleChangeDir([1, 0]) }}
+        handleRightButton={() => { handleChangeDir([0, 1]) }}
+        handleLeftButton={() => { handleChangeDir([0, -1]) }}
         handleMenuButton={handleMenuButton}
         handleStartButton={handleMenuButton}
       >
+        {/* GAME / GRID VIEW */}
         {grid}
+        {/* START SCREEN */}
         {(!started && !gameOver) && (
           <Stack sx={ menuStyle } alignItems='center'>
+            <SnakeTitle/><br/>
             <FormControl sx={{ display: 'flex', alignItems: 'center', paddingLeft: '5px' }}>
               <FormLabel sx={{ color: 'black !important' }}>Portals</FormLabel>
               <RadioGroup
@@ -227,9 +307,22 @@ export default function Snake () {
                 <FormControlLabel value="off" control={<Radio />} label="Off" />
               </RadioGroup>
             </FormControl>
+            <FormControl>
+              <FormLabel sx={{ color: 'black !important' }}>Game Speed</FormLabel>
+              <Slider 
+                value={speed} 
+                onChange={handleSpeedChange} 
+                valueLabelDisplay="off"
+                step={1} 
+                min={1}
+                max={gameSpeeds.length}
+                marks={gameSpeeds} 
+              />
+            </FormControl>
             <Button onClick={handleStartGame} sx={menuButtonFontStyle}>Start</Button>
           </Stack>
         )}
+        {/* GAME OVER SCREEN */}
         {gameOver && (
           <Stack sx={ menuStyle } direction='column' alignItems='center' >
             <Typography>Game Over!</Typography>
@@ -237,6 +330,7 @@ export default function Snake () {
             <Button onClick={handleStartMainMenu} sx={menuButtonFontStyle}>Exit to Menu</Button>
           </Stack>
         )}
+        {/* PAUSE MENU */}
         {pausedState && (
           <div style={ menuStyle }>
             <Typography>Paused</Typography>
